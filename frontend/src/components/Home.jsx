1b3e245b5aacda58c70f6c2d9e2bb1db0f23562c
Alyssa N <anajera@bu.edu>
2026-02-22 09:35:07 -0500

    Styled Homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
 export default function Home() {
+    
     return (
         <>
+            <div className="landingPage">
+                <h4 className="landingBadge">Welcome to Boston Cop Watch</h4>
+                <div className="landingHeaderContainer">
+                    <h1 className="landingHeader">A look into Boston's</h1>
+                    <h1 className="landingHeader">Police Activity</h1>                    
+                </div>
+
+                <h6 className="description">An interactive civic-tech platform designed to provide structured, legally responsible information about Boston Police Department (BPD) youth interactions and accountability data.
+    </h6>
+                <Link className="homePageButton" to="/Map">View Map</Link>                
+            </div>
+
+            <div>
+
+            </div>
+
         </>
     )
 }
