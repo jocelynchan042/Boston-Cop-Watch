907176e2a608277467750a46c6f48259b52810f6
sophia46 <sophia@espoc.com>
2026-02-22 04:10:17 -0500

    initial commit with working map of boston

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import MapWithStats from "./components/MapWithStats.jsx";
+
+function App() {
+
+  return (
+      <MapWithStats />
+  );
+}
+
+export default App
---
+++ frontend/src/components/MapWithStats.jsx
@@ -0,0 +1,17 @@
+import BostonMapClean from "./BostonMapClean.jsx";
+import {useState} from "react";
+
+export default function MapWithStats() {
+    const [selectedZip, setSelectedZip] = useState(null);
+
+    return (
+        <div style={{ display: "flex", height:"100vh"}}>
+            <BostonMapClean onZipSelect={setSelectedZip} />
+
+            <div style={{ padding: "5%", width: "30%" }}>
+                <h2>Selected ZIP</h2>
+                <p>{selectedZip || "Click a ZIP code"}</p>
+            </div>
+        </div>
+    )
+}
