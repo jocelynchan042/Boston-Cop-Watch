77d5961a6df48e3b569b6afa3ec9cb9f77bf0ed5
Alyssa N <anajera@bu.edu>
2026-02-22 10:27:21 -0500

    fix size issues on navbar and home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,8 @@ export default function Header() {
     return(
         <>
             <header className="header">
-                <div className="headerLeft">
+                <Link className="headerLeft" to="/">
+                
                     <div className="logo">
                         <img src="\BosCopWatchLogo.png" alt="Boston Cop Watch Logo" className="logoImage"/>
                     </div>
@@ -13,12 +14,12 @@ export default function Header() {
                         <div className="logoTitle">Boston Cop Watch</div>
                         <div className="logoSub">Youth Focused · Know Your Rights · Boston, MA</div>
                     </div>
-                </div>
+                </Link>
                 <div className="headerRight">
                     <nav>
-                        <Link className="mapBtn" to="/">Home</Link>
-                        <Link className="mapBtn" to="/chat">Chat</Link>
-                        <Link className="mapBtn" to="/map">Map</Link>
+                        <Link className="navButton" to="/">Home</Link>
+                        <Link className="navButton" to="/chat">Chat</Link>
+                        <Link className="navButton" to="/map">Map</Link>
                     </nav>
 
                     <div className="disclaimer">
